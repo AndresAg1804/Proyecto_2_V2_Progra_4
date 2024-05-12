961143bdaeafcb4c21c43203fb2c28306f7ef28e
AndresAg1804 <anner.angulo.gutierrez@est.una.ac.cr>
2024-05-11 23:37:11 -0600

    Inicio

---
+++ src/main/java/com/example/proyecto_2_v2/Proyecto2V2Application.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_2_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2V2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto2V2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyecto_2_v2/Proyecto2V2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_2_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto2V2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c0dc83087387d8ca90b79bdfc1e3439333dd61c1
jpce <jpcartinvillalobos@outlook.com>
2024-05-13 15:52:02 -0600

    one more thing

--- src/main/java/com/example/proyecto_2_v2/presentation/Usuarios/Controller.java
+++ src/main/java/com/example/proyecto_2_v2/presentation/Usuarios/Controller.java
@@ -1,11 +1,20 @@
 package com.example.proyecto_2_v2.presentation.Usuarios;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/")
 public class Controller {
+
+    @GetMapping("")
+    public String  show() {
+        return "redirect:/index";
+    }
+
 
 }

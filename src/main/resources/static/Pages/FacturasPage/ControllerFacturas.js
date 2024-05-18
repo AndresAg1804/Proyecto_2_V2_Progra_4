0b5be3b54c063510c4eaab01720ddac4331c3b6e
NachoBonilla03 <ignacio.bonilla.rojas@est.una.ac.cr>
2024-05-17 22:01:38 -0600

    Facturas page intialized 3

--- src/main/resources/static/Pages/FacturasPage/ControllerFacturas.js
+++ src/main/resources/static/Pages/FacturasPage/ControllerFacturas.js
@@ -1,4 +1,6 @@
-var apiC = backend + "/facturas";
+document.addEventListener("DOMContentLoaded",MenuMAINRENDER);
+
+var apiFacturas = backend + "/facturas";
 
 var stateFac = {
     list: new Array(),
@@ -6,7 +8,7 @@ var stateFac = {
     mode: "" // ADD, EDIT
 }
 
-document.addEventListener("DOMContentLoaded",MenuMAINRENDER);
+
 async function MenuMAINRENDER(event) {
     try{ await mainrender();
     await renderSearch();
@@ -14,21 +16,43 @@ async function MenuMAINRENDER(event) {
     await one();
 }
 
-function renderSearch(){
- html=` <form class="producto-buscar">
+function renderSearch() {
+    let request = new Request(apiFacturas + `findAll?idP=${loginstate.idP}`, {method: 'GET'});
+    (async ()=>{
+        const response = await fetch(request);
+        if (!response.ok) {errorMessage(response.status);return;}
+        stateFac.list = await response.json();
+        render_list();
+    })();
+
+    document.getElementById('busqueda').innerHTML = ` <form class="producto-buscar">
                 <div class="form-group-buscar">
                     <label for="Numfact">Numfact:</label>
                     <input type="text" name="numFact" id="Numfact" value="">
                     <button type="submit" id="searchCli">Buscar</button>
                 </div>
             </form>`;
-    document.getElementById('busqueda').innerHTML = html;
     document.getElementById('searchCli').addEventListener('click', search);
 
 
 }
 
 function search(){
+    numFactura= document.getElementById("Numfact").value;
+    let request = new Request(apiFacturas + `search?idP=${loginstate.idP}?numFact=${numFactura}`, {method: 'GET'});
+    (async ()=>{
+        const response = await fetch(request);
+        if (!response.ok) {errorMessage(response.status);return;}
+        stateFac.list = await response.json();
+        render_list();
+    })();
 
 }
+
+function render_itemFactura(){
+    
+}
+
+
+
 

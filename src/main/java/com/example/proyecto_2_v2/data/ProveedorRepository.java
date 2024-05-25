4fcfb7b52ba36c49e83f8bc3bf15e6fb44ea1d92
jpce007 <150888284+jpce007@users.noreply.github.com>
2024-05-24 23:36:29 -0600

    Edit Usarios-Provedores Listo ahorra se puede ono o el otro

--- src/main/java/com/example/proyecto_2_v2/data/ProveedorRepository.java
+++ src/main/java/com/example/proyecto_2_v2/data/ProveedorRepository.java
@@ -2,9 +2,16 @@ package com.example.proyecto_2_v2.data;
 
 
 import com.example.proyecto_2_v2.logic.Proveedores;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 @Repository
 public interface ProveedorRepository extends CrudRepository<Proveedores, String>{
-
+    @Transactional
+    @Modifying
+    @Query("update Proveedores p set p.nombreP = ?1  where p.idP = ?2")
+    void update_nombreP_Proveedor(String nombreP,String idPr);
 }
--- src/main/java/com/example/proyecto_2_v2/data/usuariosRepository.java
+++ src/main/java/com/example/proyecto_2_v2/data/usuariosRepository.java
@@ -1,8 +1,11 @@
 package com.example.proyecto_2_v2.data;
 
 import com.example.proyecto_2_v2.logic.Usuarios;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,5 +14,11 @@ import java.util.List;
 public interface usuariosRepository extends CrudRepository<Usuarios, String> {
     Usuarios findByUsernAndPasw(String usern,String pasw);
     Usuarios findByUsern(String usern);
+    @Query("update Usuarios p set p.pasw = ?1 where p.usern = ?2")
+    void update_pasw_Usuarios(String pasw, String usern);
 
+    @Transactional
+    @Modifying
+    @Query("UPDATE Usuarios u SET u.pasw = ?1 WHERE u.usern = ?2")
+    void updateByUsernAndPasw(String newPassword, String username);
 }
